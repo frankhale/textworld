565d3d4fe1b305ca0c03426a40a63d6ecd799f71
Frank Hale <frankhale@gmail.com>
2024-09-05 08:20:05 -0400

    UI changes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { InputComponent } from './input/input.component';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import { InputComponent } from "./input/input.component";
+import { OutputComponent } from "./output/output.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet, InputComponent],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  imports: [CommonModule, RouterOutlet, InputComponent, OutputComponent],
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'textworld-ui';
+  title = "textworld-ui";
 }
